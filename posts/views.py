7dc136066b110ea4cf16214e298a7aae5b3c7d7d
Stan1-0 <stankofb@gmail.com>
2024-02-26 16:36:20 +0000

    Added a new view for the post page and updated the urls.py to include the new view.

--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,6 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     return render(request, 'index.html', {'posts': posts})
+
+def post(request):
+    return render(request, 'posts.html')

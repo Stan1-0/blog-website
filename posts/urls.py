fef0dfd188c863f2a2ca87be2012e5322e796469
Stan1-0 <stankofb@gmail.com>
2024-02-28 13:41:57 +0000

    deleted unused templates

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
     path('post/<str:pk>',views.post, name='post')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 def index(request):
     posts = Post.objects.all()
@@ -8,3 +10,23 @@ def index(request):
 def post(request, pk):
     posts = Post.objects.get(id=pk)
     return render(request, 'posts.html', {'posts': posts})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Incorrect username or password')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')

5b2e20927165923a121c04f6901a59d082a30155
Stan1-0 <stankofb@gmail.com>
2024-02-28 14:06:53 +0000

    added user authentication

--- posts/views.py
+++ posts/views.py
@@ -11,6 +11,30 @@ def post(request, pk):
     posts = Post.objects.get(id=pk)
     return render(request, 'posts.html', {'posts': posts})
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['repeat-password']
+        
+        if password == password1:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email is registered to another account')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Sorry, username has already been taking. Please consider a different one')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
+
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
